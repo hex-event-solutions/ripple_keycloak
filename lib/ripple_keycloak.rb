adaaadd60463faa648f0ed8502222cf69d777df9
Ollie Nye <ollie@nyemail.co.uk>
2020-06-09 19:23:06 +0100

    Add rubocop

--- lib/ripple_keycloak.rb
+++ lib/ripple_keycloak.rb
@@ -1,6 +1,9 @@
-require "ripple_keycloak/version"
+# frozen_string_literal: true
+
+require 'ripple_keycloak/version'
+require 'ripple_keycloak/client'
+require 'ripple_keycloak/configuration'
 
 module RippleKeycloak
   class Error < StandardError; end
-  # Your code goes here...
 end
