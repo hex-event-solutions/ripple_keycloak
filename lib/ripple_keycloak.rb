90b5605983fbbee5de08588ce94ec2aa4a481fd9
Ollie Nye <ollie@nyemail.co.uk>
2020-06-16 18:31:10 +0100

    Extend errors off the root RippleKeycloak::Error error

--- lib/ripple_keycloak.rb
+++ lib/ripple_keycloak.rb
@@ -2,16 +2,20 @@
 
 require 'ripple_keycloak/version'
 require 'ripple_keycloak/client'
+require 'ripple_keycloak/base_model'
 require 'ripple_keycloak/group'
 require 'ripple_keycloak/role'
 require 'ripple_keycloak/user'
 
 module RippleKeycloak
   class Error < StandardError; end
-  class UnauthorizedClientError < StandardError; end
-  class UnauthorizedError < StandardError; end
-  class RealmDoesNotExistError < StandardError; end
-  class GroupNotFoundError < StandardError; end
-  class RoleNotFoundError < StandardError; end
-  class MissingPropertyError < StandardError; end
+  class UnauthorizedClientError < Error; end
+  class UnauthorizedError < Error; end
+  class RealmDoesNotExistError < Error; end
+  class GroupNotFoundError < Error; end
+  class RoleNotFoundError < Error; end
+  class MissingPropertyError < Error; end
+  class NotFoundError < Error; end
+
+  class ConflictError < Error; end
 end
